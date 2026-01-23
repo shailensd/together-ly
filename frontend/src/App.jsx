3139986fc5385e8474d460731394e5264c84d03b
shailensd <155351004+shailensd@users.noreply.github.com>
2026-01-23 12:52:47 -0800

    Optimize code for routing based on authorization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,22 @@
-import { SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/clerk-react';
+import { useAuth } from '@clerk/clerk-react';
 import { Route, Routes, Navigate } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
 import * as Sentry from "@sentry/react";
+import { use } from 'react';
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 const App = () => {
+  const { isSignedIn } = useAuth();
   return (
     <>
-      <SignedIn>
-        <SentryRoutes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<Navigate to="/" replace />} />
-        </SentryRoutes>
-      </SignedIn>
-      <SignedOut>
-        <SentryRoutes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="*" element={<Navigate to="/auth" replace />} />
-        </SentryRoutes>
-      </SignedOut>
+
+      <SentryRoutes>
+        <Route path="/" element={isSignedIn ? <HomePage /> : <Navigate to="/auth" replace />} />
+        <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
+
+        <Route path="*" element={isSignedIn ? <Navigate to="/" replace /> : <Navigate to="/auth" replace />} />
+      </SentryRoutes>
     </>
   );
 };
