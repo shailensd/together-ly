c5114bbf3264b13f531f9ab2fe140f320cd5e6ef
shailensd <155351004+shailensd@users.noreply.github.com>
2026-01-23 13:39:33 -0800

    Setup tailwaindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,24 @@ import { useAuth } from '@clerk/clerk-react';
 import { Route, Routes, Navigate } from 'react-router';
 import HomePage from './pages/HomePage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
+import CallPage from './pages/CallPage.jsx';
 import * as Sentry from "@sentry/react";
 import { use } from 'react';
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 const App = () => {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return null; // or a loading spinner
+  }
   return (
     <>
 
       <SentryRoutes>
         <Route path="/" element={isSignedIn ? <HomePage /> : <Navigate to="/auth" replace />} />
         <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
+        <Route path="/call/:id" element={isSignedIn ? <CallPage /> : <Navigate to="/auth" replace />} />
 
         <Route path="*" element={isSignedIn ? <Navigate to="/" replace /> : <Navigate to="/auth" replace />} />
       </SentryRoutes>
---
+++ frontend/src/pages/CallPage.jsx
@@ -0,0 +1,9 @@
+const CallPage = () => {
+    return (
+        <div>
+            <h1>This is the Call Page</h1>
+        </div>
+    );
+};
+
+export default CallPage;
